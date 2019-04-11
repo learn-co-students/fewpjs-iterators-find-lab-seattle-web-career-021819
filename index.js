839159db07cc317da4d68b911bc8008ae68b4dc3
ytsuji27 <ytsuji91@gmail.com>
2019-04-11 12:31:52 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let result = arr.find(game => game["result"] === "W");
+  if (result) {
+    return result["year"];
+  } else {
+    return undefined;
+  }
+}
