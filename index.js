daccca1e3827969936618ce9b4628523e837604b
github username <github email address>
2019-04-12 10:57:12 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let r = record.find( record => {
+    return record.result === "W"
+  })
+  if (r !== undefined) {
+    return r.year
+  } else {
+    return undefined
+  }
+}
