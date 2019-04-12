0bd7989ed5d69e108dde2ade75cd6349d9fd1315
Joshua Loran <joshua.loran@gmail.com>
2019-04-12 08:58:59 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let result = arr.find(e => e.result === 'W')
+  if(result === undefined){
+    return result
+  }
+    return result.year
+}
